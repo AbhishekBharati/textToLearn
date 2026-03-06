16d2924fdf6693fe331fe2b5f1bdf4ace8a32a9d
AbhishekBharati <bharatiabhishek30@gmail.com>
2026-03-06 15:17:36 +0530

    Installed and setted up Routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SampleDarkMode } from "./components/SampleDarkMode.tsx"
 import './App.css'
 
 function App() { 
 
-  return <>
-    <SampleDarkMode />
-  </>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SampleDarkMode />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
