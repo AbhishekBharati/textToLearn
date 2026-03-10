1317745bbedc4f12e1ee3da830c2b6e37b568aed
AbhishekBharati <bharatiabhishek30@gmail.com>
2026-03-11 00:55:11 +0530

    Removed the default CSS and added Routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { SampleDarkMode } from "./components/SampleDarkMode.tsx"
+import { HomePage } from './pages/HomePage.tsx';
+import { Layout } from './components/Layout.tsx';
 import './App.css'
 
 function App() { 
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SampleDarkMode />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          {/* Add more pages here as needed */}
+        </Route>
       </Routes>
     </BrowserRouter>
   )
