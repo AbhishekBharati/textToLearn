efca865af7a25e6356cefdba65ed79e279b586a8
AbhishekBharati <bharatiabhishek30@gmail.com>
2026-03-14 23:27:11 +0530

    Added SaveCourse Button

---
+++ client/src/components/SaveCourse.tsx
@@ -0,0 +1,9 @@
+import { IconBookmark } from '@tabler/icons-react';
+
+export const SaveCourse = () => {
+  return (
+    <div className="dark:text-white">
+      <IconBookmark stroke={2} />
+    </div>
+  )
+}
